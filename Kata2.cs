1c85d1cda5d5f1c27ac551aed413fb5b112ae864
filln <ulvprog@gmail.com>
2022-11-09 18:54:12 +0300

    .

--- Kata2.cs
+++ Kata2.cs
@@ -10,10 +10,37 @@ namespace Codewars0
     {
         public static int TrailingZeros(int n)
         {
-            //Solution
+            //https://www.youtube.com/watch?v=9GhiTWjhB60
+            //https://brilliant.org/wiki/trailing-number-of-zeros/
+            int count = 0;
+            int multiplier = 5;
+            while (true)
+            {
+                int currentCoutn = n / multiplier;
+                if (currentCoutn.Equals(0))
+                {
+                    break;
+                }
+                count += currentCoutn;
+                multiplier *= 5;
+            }
+            return count;
+            /*
+             *   public static int TrailingZeros(int n)
+                  { 
+                    int fives = 0;
+                    for (int i = 5; i <= n; i *= 5)
+                        fives += n/i;
+    
+                    return fives;
+                  }
+             */
         }
 
+        public static string sumStrings(string a, string b)
+        {
 
+        }
 
 
 
--- Program.cs
+++ Program.cs
@@ -9,8 +9,10 @@ namespace Codewars0
             Console.WriteLine("Hello World!");
             ///////////////////////////////////////////
             
-            int[][] matrix = new int[][] { new[] { 2, 5, 3 }, new[] { 1, -2, -1 }, new[] { 1, 3, 4 } };
-            int result = Kata1.Matrix.Determinant(matrix);
+            //int[][] matrix = new int[][] { new[] { 2, 5, 3 }, new[] { 1, -2, -1 }, new[] { 1, 3, 4 } };
+
+            int result = Kata2.TrailingZeros(531);
+            Console.WriteLine("result == ");
             Console.WriteLine(result);
 
             ///////////////////////////////////////////
