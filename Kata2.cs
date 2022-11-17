e6ea8750e6338b43fd665db338bfc8be56b336a9
filln <ulvprog@gmail.com>
2022-11-17 22:37:07 +0300

    .

--- Kata2.cs
+++ Kata2.cs
@@ -39,9 +39,53 @@ namespace Codewars0
 
         public static string sumStrings(string a, string b)
         {
-            
-        }
+            int minLength, maxLength;
+            StringBuilder minString, maxString;
+            if (a.Length >= b.Length)
+            {
+                minLength = b.Length;
+                maxLength = a.Length;
+                minString = new StringBuilder(b);
+                maxString = new StringBuilder(a);
+            }
+            else
+            {
+                minLength = a.Length;
+                maxLength = b.Length;
+                minString = new StringBuilder(a);
+                maxString = new StringBuilder(b);
+            }
 
+            int zerosCount = maxLength - minLength;
+            if (zerosCount > 0)
+            {
+                StringBuilder zeros = new();
+                for (int i = 0; i < zerosCount; i++)
+                {
+                    zeros.Append('0');
+                }
+                minString = zeros.Append(minString);
+            }
+
+            int add = 0;
+            StringBuilder result = new();
+            for (int index = maxLength - 1; index >= 0; index--)
+            {
+                int digitsSum = Convert.ToInt32(maxString[index]) + Convert.ToInt32(minString[index]) + add;
+                if (digitsSum > 9)
+                {
+                    add = 1;
+                    digitsSum %= 10;
+                }
+                else
+                {
+                    add = 0;
+                }
+                result.Append(digitsSum);
+            }
+
+            return result.ToString().ToCharArray().Reverse().ToString();
+        }
 
 
 
