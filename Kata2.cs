20b9861810eecf8e9c07df8f38e28ffdce8b4480
filln <ulvprog@gmail.com>
2022-11-25 15:22:17 +0300

    .

--- Kata2.cs
+++ Kata2.cs
@@ -227,6 +227,33 @@ namespace Codewars0
                 }
                 return resultList.ToArray();
             }
+
+            /*
+             * 
+             * public class SnailSolution
+                {
+                    public static int[] Snail(int[][] array)
+                    {
+                        int l = array[0].Length;
+                        int[] sorted = new int[l * l];
+                        Snail(array, -1, 0, 1, 0, l, 0, sorted);
+                        return sorted;
+                    }
+
+                    public static void Snail(int[][] array, int x, int y, int dx, int dy, int l, int i, int[] sorted)
+                    {
+                        if (l == 0)
+                            return;
+                        for (int j = 0; j < l; j++)
+                        {
+                            x += dx;
+                            y += dy;
+                            sorted[i++] = array[y][x];
+                        }
+                        Snail(array, x, y, -dy, dx, dy == 0 ? l - 1 : l, i, sorted);
+                    }
+                }
+             */
         }
 
 
