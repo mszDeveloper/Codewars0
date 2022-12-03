531240e354062c5e201a76a469b96c0648031b79
filln <ulvprog@gmail.com>
2022-12-03 22:09:43 +0300

    .

--- Kata3.cs
+++ Kata3.cs
@@ -9,10 +9,62 @@ namespace Codewars0
     class Kata3
     {
 
-        //https://www.codewars.com/kata/56a6ce697c05fb4667000029/train/csharp
-        public static int NextPal(int val)
+        class NextPalindrom
         {
-            return val;
+        //https://www.codewars.com/kata/56a6ce697c05fb4667000029/train/csharp
+            static int GetLeftSubNumber(int val)
+            {
+                string valStr = val.ToString();
+                int digitCount = valStr.Length;
+                string valStrLeft = valStr.Substring(0, digitCount / 2);
+                return int.Parse(valStrLeft);
+            }
+            static int GetReverseRightSubNumber(int val)
+            {
+                string valStr = val.ToString();
+                int digitCount = valStr.Length;
+                string valStrRight;
+                if ((digitCount % 2).Equals(0))
+                {
+                    valStrRight = valStr.Substring(digitCount / 2, digitCount / 2);
+                }
+                else
+                {
+                    valStrRight = valStr.Substring(digitCount / 2 + 1, digitCount / 2);
+                }
+                return int.Parse(Reverse(valStrRight));
+            }
+            static string Reverse(string str)
+            {
+                char[] charArray = str.ToCharArray();
+                Array.Reverse(charArray);
+                return new string(charArray);
+            }
+            public static int NextPal(int val)
+            {
+                while (true)
+                {
+                    val++;
+                    if (GetLeftSubNumber(val).Equals(GetReverseRightSubNumber(val)))
+                    {
+                        return val;
+                    }
+                }            
+            }
+            //12
+            //123
+            //121
+            //1234
+            //12345        
+            /**
+             *   
+             *  public static int NextPal(int val) {
+                for (int i = val + 1; i < int.MaxValue; i++) {
+                  var digits = i.ToString().ToCharArray();
+                  Array.Reverse(digits);
+                  if (i.ToString() == new string(digits)) return i;
+                }
+             */
         }
 
 
