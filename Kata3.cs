45948e039948d702142c65974127c46415369b9a
mszgit <mszgit@gmail.com>
2022-12-03 07:35:38 +0300

    .

---
+++ Kata3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars0
+{
+    class Kata3
+    {
+
+        //https://www.codewars.com/kata/56a6ce697c05fb4667000029/train/csharp
+        public static int NextPal(int val)
+        {
+            return val;
+        }
+
+
+
+
+
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,9 +11,9 @@ namespace Codewars0
             
             //int[][] matrix = new int[][] { new[] { 1, 2, 3 }, new[] { 4, 5, 6 }, new[] { 7, 8, 9 } };
             //int[] result = Kata2.SnailSolution.Snail(matrix);
-            int result = Kata2.SumOfSquares.NSquaresFor(3456);
+            //int result = Kata2.SumOfSquares.NSquaresFor(3456);
             Console.WriteLine("result == ");
-            Console.WriteLine(result);
+            //Console.WriteLine(result);
 
             ///////////////////////////////////////////
             Console.WriteLine("Ready!");
