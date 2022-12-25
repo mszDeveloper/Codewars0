d4872e04956e5ed4ed663d11df667cf820d11df4
mszgit <mszgit@gmail.com>
2022-12-25 21:17:46 +0300

    .

--- Kata6.cs
+++ Kata6.cs
@@ -16,19 +16,69 @@ namespace Codewars0
                 return signature[n];
             }
             int[] result = new int[n + 1];
+            Array.Copy(signature, result, signature.Length);
             int sum = 0;
             for (int i = 0; i <= n - signature.Length; i++)
             {
                 sum = 0;
-                for (int k = i; k < indexes.Length; k++)
+                for (int k = 0; k < indexes.Length; k++)
                 {
-                    sum += signature[indexes[k]];
+                    sum += result[indexes[k] + i];
                 }
+                result[signature.Length + i] = sum;
             }
+            return result[result.Length - 1];
 
         }
 
+        //https://www.codewars.com/kata/57a1d5ef7cb1f3db590002af
+        public static int Fib(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            int result = 0;
+            int last = 1;
+            int previous = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                result = last + previous;
+                previous = last;
+                last = result;
+            }
+            return result;
+            /**
+             *     public static int fib(int n)
+                    {
+                        int a = 0;
+                        int b = 1;
+        
+                        for (int i = 0; i < n; i++)
+                        {
+                          int temp = a;
+                          a = b;
+                          b = temp + a;
+                        }
+        
+                        return a;
+                    }
 
+                    public static int fib(int n)
+                        {
+                               if (n==1||n==2)
+                                {
+                                    return 1;
+                                }
+                                return fib(n - 1) + fib(n - 2);
+                        }
+              public static int fib(int n)
+                  {
+                    double phi = 0.5 + 0.5 * Math.Sqrt(5);
+                    return (int)Math.Round((Math.Pow(phi, n) - Math.Pow(-phi, 1 / n)) / Math.Sqrt(5));
+                  }
+             */
+        }
 
 
 
