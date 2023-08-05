50cedee18de0d409ffba215265ce175c2205a5ef
filln <ulvprog@gmail.com>
2023-08-05 20:19:47 +0300

    .

---
+++ Codewars0Py/FibonacciReloaded.py
@@ -0,0 +1,12 @@
+#https://www.codewars.com/kata/52549d3e19453df56f0000fe
+
+def fib(n):
+    a = 0
+    b = 1
+    for i in range(n - 1):
+        temp = a
+        a = b
+        b = temp + a
+    return a
+
+print(fib(10))
